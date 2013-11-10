8947830ac56a4a0ccbab3f4a99e2f49bf3ffa9d4
Guillaume <guillaume.frichet@gmail.com>
2013-11-10 16:38:18 +0100

    Affichage de LstStillAlive dans Game

--- Gladiator/Match.cs
+++ Gladiator/Match.cs
@@ -12,12 +12,12 @@ namespace Gladiator
 			set { this._resultMatch = value; }
 		}
 
-        private Team _lstStillALive;
-        public Team StillAlive
-        {
-            get { return this._lstStillALive; }
-            set { this._lstStillALive = value; }
-        }
+		private Team _StillALive;
+		public Team StillAlive
+		{
+			get { return this._StillALive; }
+			set { this._StillALive = value; }
+		}
 
 		// MATCH
 		public Match(Team t1, Team t2)
@@ -27,28 +27,28 @@ namespace Gladiator
 			int i = 0, j = 0, nbRound = 1;
 			while (t1.ChampList[2].IsAlive && t2.ChampList[2].IsAlive && i <= 3 && j <= 3) {
 				ResultMatch += "\n----- ROUND " + nbRound + " - " + t1.ChampList[i].Name + " vs " + t2.ChampList[j].Name + " -----\n";
-                if (t1.ChampList[i].IsAlive && t2.ChampList[j].IsAlive)
-                {
+				if (t1.ChampList[i].IsAlive && t2.ChampList[j].IsAlive)
+				{
 					currentRound = new Round(t1.ChampList[i], t2.ChampList[j]);
 					ResultMatch += currentRound.Result;
-                    nbRound++;
-                }
-				
-                if (!t1.ChampList[i].IsAlive)
-                    i++;
-                if (!t2.ChampList[j].IsAlive)
-                    j++;
+					nbRound++;
+				}
+
+				if (!t1.ChampList[i].IsAlive)
+					i++;
+				if (!t2.ChampList[j].IsAlive)
+					j++;
 				//Console.WriteLine(t1.ChampList[i].Name + " : " + t1.ChampList[i].IsAlive.ToString());
 				//Console.WriteLine(t2.ChampList[j].Name + " : " + t2.ChampList[j].IsAlive.ToString());
 			}
 			if (i > j) {
 				ResultMatch += t2.Name + " gagne le match contre " + t1.Name;
-                StillAlive = t2;
+				StillAlive = t2;
 				t2.NbWin++;
 				t1.NbLose++;
 			} else if (i < j) {
 				ResultMatch += t1.Name + " gagne le match contre " + t2.Name;
-                StillAlive = t1;
+				StillAlive = t1;
 				t1.NbWin++;
 				t2.NbLose++;
 			} else if (i == j) {
@@ -59,14 +59,14 @@ namespace Gladiator
 
 			// On rez les champions après chaque combat d'équipe
 			// -> ils recombatent dans les prochains matchs et on ne peut pas tester la dernière équipe vivante comme avant
-            foreach (Champion c in t1.ChampList)
-            {
-                c.IsAlive = true;
-            }
-            foreach (Champion c in t2.ChampList)
-            {
-                c.IsAlive = true;
-            }
+			foreach (Champion c in t1.ChampList)
+			{
+				c.IsAlive = true;
+			}
+			foreach (Champion c in t2.ChampList)
+			{
+				c.IsAlive = true;
+			}
 
 		}
 	}
